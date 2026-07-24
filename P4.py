77156e50271fd87f4454b35e04397aa58cbedf8e
Vaishnavi <akulavaishnavi711@gmail.com>
2026-07-24 21:05:26 +0530

    Add P4.py

---
+++ P4.py
@@ -0,0 +1,16 @@
+
+
+
+import numpy as np
+import numpy as np
+
+a = np.array([10, 20, 30, 40, 50])
+
+print(a)
+import numpy as np
+
+a = np.array([10, 20, 30, 40, 50])
+
+print(a[0])
+print(a[2])
+print(a[-1])
